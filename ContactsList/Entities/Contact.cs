dcddad88d44405a564c06145d3735d668dbfbdf7
Fernando Cisneros <fernando.saxofone@gmail.com>
2016-11-09 14:29:38 -0200

    initial commit

---
+++ ContactsList/Entities/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SQLite;
+
+namespace ContactsList
+{
+	public class Contact
+	{
+
+		[PrimaryKey, AutoIncrement]
+		public long Id { get; set; }
+		[MaxLength(250)]
+		public string Name { get; set; }
+		[MaxLength(250)]
+		public string Email { get; set; }
+		[MaxLength(250)]
+		public string Image { get; set; }
+		
+	}
+}
---
+++ ContactsList/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+using SQLite;
+
+namespace ContactsList
+{
+	[Activity(Label = "ContactsList", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			this.CreateDatabase();
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+		}
+
+
+		private void CreateDatabase() {
+			string dbPath = Path.Combine(
+				System.Environment.GetFolderPath( System.Environment.SpecialFolder.Personal ),
+				"contacts_list.db3"
+			);
+			System.Diagnostics.Debug.WriteLine(dbPath);
+
+			var db = new SQLiteConnection(dbPath);
+			db.CreateTable<Contact>();//create Contact table
+
+
+		}
+
+
+	}
+}
+
