874864f2ce49e8d98b181cc3ff5c7ff0e064a30d
Fernando Cisneros <fernando.saxofone@gmail.com>
2016-11-10 02:01:59 -0200

    added image profile

--- ContactsList/NewContactActivity.cs
+++ ContactsList/NewContactActivity.cs
@@ -18,10 +18,16 @@ namespace ContactsList
 	public class NewContactActivity : Activity
 	{
 
+		public const int PickImageId = 1000;
+
+		private string ImageURI = null;
+
 		EditText nameEditText;
 		EditText emailEditText;
-		Button saveContactButton;
+		Button saveContactButton, pickImageButton;
 		Contact contact;
+		ImageView contactImageView;
+
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -35,9 +41,11 @@ namespace ContactsList
 			nameEditText = FindViewById<EditText>(Resource.Id.nameEditText);
 			emailEditText = FindViewById<EditText>(Resource.Id.emailEditText);
 			saveContactButton = FindViewById<Button>(Resource.Id.saveContactButton);
+			pickImageButton = FindViewById<Button>(Resource.Id.pickImageButton);
+			contactImageView = FindViewById<ImageView>(Resource.Id.contactImageView);
 
 			saveContactButton.Click+= SaveContactButton_Click;
-
+			pickImageButton.Click+= PickImageButton_Click;
 
 
 
@@ -66,7 +74,8 @@ namespace ContactsList
 			else {
 				contact = new Contact { 
 					Name = nameEditText.Text,
-					Email = emailEditText.Text
+					Email = emailEditText.Text,
+					Image = this.ImageURI
 				};
 				Database.db.Insert(contact);
 				ContactsAdapter.Add(contact);
@@ -76,5 +85,36 @@ namespace ContactsList
 			}
 
 		}
+
+		void PickImageButton_Click(object sender, EventArgs e)
+		{
+
+			Intent intent = new Intent();
+			intent.SetType("image/*");
+			intent.SetAction(Intent.ActionGetContent);
+			StartActivityForResult(Intent.CreateChooser(intent, "Select Picture"), PickImageId);
+
+		}
+
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult(requestCode, resultCode, data);
+
+			if (resultCode != Result.Ok) {
+				return;
+			}
+
+			switch (requestCode) {
+				case PickImageId:
+					Android.Net.Uri uri = data.Data;
+					contactImageView.SetImageURI(uri);
+					this.ImageURI = uri.ToString();
+					break;
+				default:
+					break;
+			}
+		}
+
 	}
 }
